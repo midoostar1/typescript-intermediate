1ee93ce13e95bc62a2c670d8d42b84f8c63a080e
midoostar1 <95588511+midoostar1@users.noreply.github.com>
2023-03-29 23:07:15 -0500

    implementing interfaces in typescript

---
+++ dist/second.js
@@ -0,0 +1,20 @@
+"use strict";
+//class have to implement all fields of the interface its implementing
+class Instagram {
+    constructor(cameraMode, filter, burst) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+}
+class Youtube {
+    constructor(cameraMode, filter, burst, shorts) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+        this.shorts = shorts;
+    }
+    createStory() {
+        console.log('story');
+    }
+}
